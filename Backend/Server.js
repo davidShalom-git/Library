26546edef3628f134e3bd75d45de4a12298afd10
davidshalom-git <davidshalom2609@gmail.com>
2025-08-27 13:56:02 +0530

    Lib

--- Backend/Server.js
+++ Backend/Server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(ClerkExpressWithAuth());
 
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log('✅ Connected to MongoDB')).catch((err) => {
-  console.error('❌ MongoDB connection error:', err);
+  console.error('❌ MongoDB connection error:', err)
 
 });
 
